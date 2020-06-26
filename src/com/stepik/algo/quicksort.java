4f1390eea17d4246bb4c7147b1e05513b7115ebd
Drauggy <60293999+Drauggy@users.noreply.github.com>
2020-06-26 17:36:07 +0300

    Update quicksort.java

--- src/com/stepik/algo/quicksort.java
+++ src/com/stepik/algo/quicksort.java
@@ -2,9 +2,8 @@ package com.stepik.algo;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
-public class QuickSort {
+public class quicksort {
     private int[] quickSort(int[] A, int l, int r) {
         if (l >= r) return A;
         int m = Partition(A, l, r);
@@ -12,6 +11,7 @@ public class QuickSort {
         quickSort(A, m + 1, r);
         return A;
     }
+
     private int[] quickSortR(int[] A, int l, int r) {
         if (l >= r) return A;
         Random random = new Random();
@@ -20,9 +20,56 @@ public class QuickSort {
         A[randomElement] = A[l];
         A[l] = a;
         int m = Partition(A, l, r);
-        quickSort(A, l, m);
-        quickSort(A, m + 1, r);
+        quickSortR(A, l, m);
+        quickSortR(A, m + 1, r);
         return A;
+    }
+/*
+Quicksort3 - быстрая сортировка с выбором случайного разделителя и элиминацией хвостовой рекурсии
+ */
+    private int[] quickSortREL(int[] A, int l, int r) {
+        while ( l < r) {
+
+
+            Random random = new Random();
+            int randomElement = random.nextInt(r - l) + l;
+            int a = A[randomElement];
+            A[randomElement] = A[l];
+            A[l] = a;
+            int m = Partition(A, l, r);
+            if (m - 1 - l < r - (m + 1)) {
+                quickSort3(A, l, m);
+                l = m + 1;
+            } else {
+                quickSort3(A, m + 1, r);
+                r = m;
+
+            }
+        }
+            return A;
+
+    }
+    private int[] quickSort3(int[] A, int l, int r) {
+        while ( l < r) {
+
+
+            Random random = new Random();
+            int randomElement = random.nextInt(r - l) + l;
+            int a = A[randomElement];
+            A[randomElement] = A[l];
+            A[l] = a;
+            int[] m = Partition3(A, l, r);
+            if (m[0] - 1 - l < r - (m[1] + 1)) {
+                quickSort3(A, l, m[0]);
+                l = m[1] + 1;
+            } else {
+                quickSort3(A, m[1] + 1, r);
+                r = m[0];
+
+            }
+        }
+        return A;
+
     }
 
     private int Partition(int[] A, int l, int r) {
@@ -41,17 +88,45 @@ public class QuickSort {
         A[j] = temp;
         return j;
     }
+    private int[] Partition3(int[] A, int l, int r) {
+        int x = A[l];
+        int j = l;
+        int k = j;
+        for (int i = l + 1; i < r; i++) {
+            if (A[i] < x) {
+                j++;
+                k++;
+                int temp = A[i];
+                A[i] = A[k];
+                A[k] = temp;
+                temp = A[k];
+                A[k] = A[j];
+                A[j] = temp;
+            }
+            else if ( A[i] == x) {
+                k++;
+                int temp = A[i];
+                A[i] = A[k];
+                A[k] = temp;
+            }
+        }
+        int temp = A[l];
+        A[l] = A[j];
+        A[j] = temp;
+        return new int[]{j,k};
+    }
+
 
     private void Test() {
         Random random = new Random();
-        int[] testMasssive = new int[40];
+        int[] testMasssive = new int[500];
         for (int j = 0; j < 100; j++) {
 
 
-            for (int i = 0; i < 40; i++) {
-                testMasssive[i] = random.nextInt(100);
+            for (int i = 0; i < 500; i++) {
+                testMasssive[i] = random.nextInt(8);
             }
-            int[] A = new QuickSort().quickSortR(testMasssive, 0, testMasssive.length);
+            int[] A = new quicksort().quickSort3(testMasssive, 0, testMasssive.length);
             int[] B = Arrays.stream(testMasssive)
                     .sorted().toArray();
 
@@ -82,7 +157,7 @@ public class QuickSort {
 //        System.out.println(Arrays.toString(new QuickSort().quickSort(massive, 0, massive.length)));
 //        //long end = System.currentTimeMillis();
 //        //System.out.println("Время исполнения: " + (end - start));
-         new QuickSort().Test();
+        new quicksort().Test();
 
     }
 }
